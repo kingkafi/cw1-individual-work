c3d8a7d5c4fcb65ae7c0489edfd889fa91d4d232
kingkafi <116303193+kingkafi@users.noreply.github.com>
2022-11-17 23:45:17 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// This is the lession items array, if you want to add more product for sale
+// then just add object
+export var lessons = [
+    // each object represent a product item, for more products just add more objects
+    {
+        Subject: 'Math',
+        Location: 'London',
+        Price: '£100.00',
+        Stock: 5,
+
+        // I added font awesome cdn link, this is a font awesome icon name,
+        // just paste icon name here the icon will appear in product
+        image: 'fa fa-calculator',
+    },
+    {
+        Subject: 'History',
+        Location: 'Oxford',
+        Price: '£100.00',
+        Stock: 5,
+        image: 'fa fa-history',
+    },
+    {
+        Subject: 'English',
+        Location: 'London',
+        Price: '£100.00',
+        Stock: 5,
+        image: 'fa fa-language',
+    },
+    {
+        Subject: 'Science',
+        Location: 'York',
+        Price: '£80',
+        Stock: 5,
+        image: 'fa fa-flask',
+    },
+    {
+        Subject: 'Music',
+        Location: 'Bristol',
+        Price: '£90',
+        Stock: 5,
+        image: 'fa fa-music',
+    },
+]
